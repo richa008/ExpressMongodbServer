f7056a48b107c4efde1535f3ec94660d34cd19ff
Richa <richa008@gmail.com>
2019-11-28 07:42:20 -0800

    Adding views

--- app.js
+++ app.js
@@ -27,15 +27,15 @@ connect.then((db) => {
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public'))); // serves static data from public folder
 
 app.use('/', index);
 app.use('/users', users);
@@ -44,21 +44,21 @@ app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(request, response, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, request, response, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  response.locals.message = err.message;
+  response.locals.error = request.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  response.status(err.status || 500);
+  response.render('error');
 });
 
 module.exports = app;
